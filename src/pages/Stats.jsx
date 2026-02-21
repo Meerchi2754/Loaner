04df28a6eb167e878218db52e44528320acc3936
Meerchi2754 <meetrajparmar556@gmail.com>
2026-02-21 12:24:29 +0530

    Add Changes in Stats page.

--- src/pages/Stats.jsx
+++ src/pages/Stats.jsx
@@ -1,43 +1,43 @@
-// import { useSelector } from "react-redux";
-// import { useState, useEffect } from "react";
-// import {db} from "../db/appDB";
-// const Stats = () => {
-//   const [categoriesList, setCategoriesList] = useState([]);
+import { useSelector } from "react-redux";
+import { useState, useEffect } from "react";
+import { db } from "../db/appDB";
+const Stats = () => {
+  //   const [categoriesList, setCategoriesList] = useState([]);
 
-//   useEffect(() => {
-//     let mounted = true;
-//     (async () => {
-//       try {
-//         const tx = await db.categories.toArray();
-//         console.log("Loaded transactions:", tx);
-//         if (mounted) setCategoriesList(tx);
-//       } catch (err) {
-//         console.error("Failed to load transactions:", err);
-//       }
-//     })();
-//     return () => {
-//       mounted = false;
-//     };
-//   }, []);
+  //   useEffect(() => {
+  //     let mounted = true;
+  //     (async () => {
+  //       try {
+  //         const tx = await db.categories.toArray();
+  //         console.log("Loaded transactions:", tx);
+  //         if (mounted) setCategoriesList(tx);
+  //       } catch (err) {
+  //         console.error("Failed to load transactions:", err);
+  //       }
+  //     })();
+  //     return () => {
+  //       mounted = false;
+  //     };
+  //   }, []);
 
-//   console.log("Categories list in Stats component:", categoriesList);
+  //   console.log("Categories list in Stats component:", categoriesList);
 
-//   // const categories = useSelector((state) => state.transactions.categories);
-//   // console.log("Categories in Stats component:", categories);
+  //   // const categories = useSelector((state) => state.transactions.categories);
+  //   // console.log("Categories in Stats component:", categories);
 
-//   if (!categoriesList || categoriesList.length === 0) {
-//     return <div>No Categories Found</div>;
-//   }
-//   return (
-//     <div>
-//       <h1>Categories</h1>
-//       <ul>
-//         {categoriesList.map((category) => (
-//           <li key={category.id}>{category.name}</li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// };
+  //   if (!categoriesList || categoriesList.length === 0) {
+  //     return <div>No Categories Found</div>;
+  //   }
+  return (
+    <div>
+      {/* <h1>Categories</h1>
+      <ul>
+        {categoriesList.map((category) => (
+          <li key={category.id}>{category.name}</li>
+        ))}
+      </ul> */}
+    </div>
+  );
+};
 
-// export default Stats;
+export default Stats;
